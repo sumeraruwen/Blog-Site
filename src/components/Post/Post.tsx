2fbf1ae8c07a6dc479f73cc2033930757485dd49
unknown <sumeraruwen@gmail.com>
2023-05-14 17:36:49 +0530

    content component

--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -1,9 +1,36 @@
 import React, { Component } from 'react'
 
-export default class Post extends Component {
+
+type PostProps = {
+    _id: string;
+    title: string;
+    description: string;
+    hoursCount?: number;
+    lecturerName?: string;
+    // tags: string[];
+    // removePostFromList?: (postId: string) => void;
+  };
+  
+  type PostState = {};
+
+export default class Post extends Component<PostProps,PostState> {
   render() {
     return (
-      <div>
+        <div className="p-6 border border-slate-400 w-full rounded mt-24">
+        <h3 className="text-center">{this.props.title}</h3>
+        {this.props.description}
+        <span className="flex space-x-5">
+          {this.props.hoursCount ? (
+            <p>
+              <strong>Hours count</strong> : {this.props.hoursCount}
+            </p>
+          ) : null}
+          {this.props.lecturerName ? (
+            <p>
+              <strong>Lecturer Name</strong> : {this.props.lecturerName}
+            </p>
+          ) : null}
+        </span>
         
       </div>
     )
--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
 import Header2 from '../../components/Header2'
+import Post from '../../components/Post'
 
 export default class Profile extends Component {
     render() {
         return (
             <div>
                 <Header2/>
+                <Post _id="fhfghfg" title="fghf" description="fghfgh" hoursCount={4} lecturerName="fghfh" />
+                <Post _id="fhfghfg" title="fghf" description="fghfgh" hoursCount={4} lecturerName="fghfh" />
                 
             </div>
         )
